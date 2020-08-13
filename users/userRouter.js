acbdc80edca2bf7d1819ac9532a0a5142bcccd54
Chris Forrest <redrider_cr@yahoo.com>
2020-08-13 15:05:45 -0400

    mvp met

--- users/userRouter.js
+++ users/userRouter.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const userDB = require('./userDb');
 const postDB = require('../posts/postDb');
-const { checkUserID } = require('../middleware/validation');
-const { checkUser } = require('../middleware/validation');
-const { checkPost } = require('../middleware/validation');
+//const { checkUserID } = require('../middleware/validation');
+//const { checkUser } = require('../middleware/validation');
+//const { checkPost } = require('../middleware/validation');
 
 const router = express.Router();
 
-router.post('/', checkUser(), (req, res) => {
+router.post('/', validateUser, (req, res) => {
   userDB.insert(req.body)
   .then((post) => {
     res.status(201).json(post)
@@ -18,7 +18,7 @@ router.post('/', checkUser(), (req, res) => {
   })
 });
 
-router.post('/:id/posts', checkPost(), checkUserID(), (req, res) => {
+router.post('/:id/posts', validatePost, validateUserId, (req, res) => {
   req.body.user_id = req.params.id;
   postDB.insert(req.body)
     .then(post => {
@@ -41,7 +41,7 @@ router.get('/', (req, res) => {
     })
 });
 
-router.get('/:id', checkUserID(), (req, res) => {
+router.get('/:id', validateUserId, (req, res) => {
   userDB.getById(req.params.id)
     .then(user => {
       res.status(200).json(user)
@@ -52,7 +52,7 @@ router.get('/:id', checkUserID(), (req, res) => {
     })
 });
 
-router.get('/:id/posts',checkUserID(), (req, res) => {
+router.get('/:id/posts', validateUserId, (req, res) => {
   userDB.getUserPosts(req.params.id)
     .then(posts => {
       res.status(200).json(posts)
@@ -63,26 +63,61 @@ router.get('/:id/posts',checkUserID(), (req, res) => {
     })
 });
 
-router.delete('/:id', (req, res) => {
-  // do your magic!
+router.delete('/:id', validateUserId, (req, res) => {
+  userDB.remove(req.params.id)
+    .then(user => {
+      res.status(200).json(user)
+    })
+    .catch(err => {
+      console.log(err.stack)
+      res.status(500).json({ message: " Cannot remove user."})
+    })
 });
 
-router.put('/:id', (req, res) => {
-  // do your magic!
+router.put('/:id', validateUserId , (req, res) => {
+  userDB.update(req.params.id , req.body)
+    .then(user => {
+      res.status(200).json(user)
+    })
+    .catch(err => {
+      console.log(err.stack)
+      res.status(500).json({ message: " Cannot update user."})
+    })
 });
 
 //custom middleware
 
 function validateUserId(req, res, next) {
-  // do your magic!
+  userDB.getById(req.params.id)
+    .then(user => {
+      if(user){
+        req.user = user
+        next()
+      }else{
+        res.status(404).json({ message: "User not found"})
+      }
+    })
+    .catch(next)
 }
 
 function validateUser(req, res, next) {
-  // do your magic!
+  if(!req.body.name){
+    return res.status(400).json({ message: "Missing required name field."});
+}
+  if(!Object.keys(req.body).length) {
+    return res.status(400).json({ message: "Missing user data."})
+}
+  next();
 }
 
 function validatePost(req, res, next) {
-  // do your magic!
+  if(!Object.keys(req.body).length){
+    return res.status(400).json({ message: "Missing post data."})
+}
+  if(!req.body.text){
+    return res.status(400).json({ message: "Missing required text field"})
+}
+  next();
 }
 
 module.exports = router;
